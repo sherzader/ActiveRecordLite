464452fc9f2f59b13734f25fdd55ef2159af9e0e
Sheri Zada <sheri.h.zada@gmail.com>
2015-11-13 12:05:00 -0800

    define attr_accessor methods

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,20 +5,21 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+
   end
 
   def self.finalize!
+
   end
 
   def self.table_name=(table_name)
-    # ...
+    table_name.tableize
   end
 
   def self.table_name
-    # ...
+    table_name.tableize
   end
-
+# ::all return an array of all records in DB
   def self.all
     # ...
   end
@@ -26,7 +27,7 @@ class SQLObject
   def self.parse_all(results)
     # ...
   end
-
+#::find look up a record by primary key
   def self.find(id)
     # ...
   end
@@ -42,15 +43,16 @@ class SQLObject
   def attribute_values
     # ...
   end
-
+# #insert a new row into the table
   def insert
     # ...
   end
-
+# #update the row with the id of this SQLObject
   def update
     # ...
   end
-
+# #save calls insert/update depending on whether SQLObject exists
+# a convenience method
   def save
     # ...
   end
